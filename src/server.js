6a44ad338dff2c0cef61c68f56af4d7d7eb97968
Rickson Thompson <ricksoncaique.tecgo@gmail.com>
2020-07-01 09:58:12 -0400

    configured deploy

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  "mongodb+srv://dev-rick:rcts1721@cluster0.obamc.mongodb.net/repository-manager?retryWrites=true&w=majority", {
+  process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
